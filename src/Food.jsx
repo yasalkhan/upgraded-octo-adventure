891e5070db3624a70f62dc6a6cbd9ffac7ed0557
yasal khan <yasalkhan89@gmail.com>
2023-08-27 02:55:56 -0700

    highdlfja

--- src/Food.jsx
+++ src/Food.jsx
@@ -1,34 +1,40 @@
 import React, { useState } from 'react';
 import './cart.css';
- import spicyImage from './images/shreyak-singh-0j4bisyPo3M-unsplash.jpg';
+import spicyImage from './images/shreyak-singh-0j4bisyPo3M-unsplash.jpg';
 
 function Food(props) {
     const [cartItems, setCartItems] = useState([]);
 
     const addToCart = () => {
-        const productName = document.getElementById("rating");
-        const productPrice = document.getElementById("pricetag"); 
+        const productName = document.getElementById("rating").textContent;
+        const productPrice = document.getElementById("pricetag").textContent;
         const cartItem = `${productName} - ${productPrice}`;
 
         setCartItems([...cartItems, cartItem]);
     };
 
     return (
-        <button className="add-to-cart"  onClick={addToCart}>   <div className="food-card">
+        <div className="food-card">
             <div className="food">
-                <img className="img" src={props.spicyImage} alt="Spicy Food" />
+                <img className="img" src={spicyImage} alt="Spicy Food" />
                 <div className="line"></div>
                 <p id='rating'>6.5 star</p>
                 <br />
                 <h1 id='pricetag'>Rs: 199</h1>
+                <button className="add-to-cart" onClick={addToCart}>
+                    Add to Cart
+                </button>
             </div>
-            
+
             <div className="cart-items">
-                {cartItems.map((item, index) => (
-                    <li key={index}>{item}</li>
-                ))}
+                <h2>Your Cart</h2>
+                <div>
+                    {cartItems.map((item, index) => (
+                        <li key={index}>{item}</li>
+                    ))}
+                </div>
             </div>
-        </div></button>
+        </div>
     );
 }
 
